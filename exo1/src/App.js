98cba25524d4f2dbf9e79200be71b2f29e4f0922
Mratsoo <mratsoo@gmail.com>
2022-09-02 09:35:23 +0200

    etape1

---
+++ exo1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Page1 from './components/page/page1';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import Cards from './components/cards/cards';
+
+function App() {
+  return (
+   <>
+<Page1/>
+<Cards/>
+   </>
+  );
+}
+
+export default App;
---
+++ exo1/src/components/page/page1.jsx
@@ -0,0 +1,11 @@
+import '../page/page.css';
+
+function Page1() {
+  return (
+    <>
+    <h1>La Boutique de Alexandre</h1>
+    </>
+  );
+}
+
+export default Page1;
