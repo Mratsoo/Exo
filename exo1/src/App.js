e66647ac9d5086d838956c47578ab932b266737f
Mratsoo <mratsoo@gmail.com>
2022-09-02 10:04:21 +0200

    exo 2

--- exo1/src/App.js
+++ exo1/src/App.js
@@ -1,15 +1,30 @@
-import './App.css';
-import Page1 from './components/page/page1';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
-import Cards from './components/cards/cards';
+import "./App.css";
+import Page1 from "./components/page/page1";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import Cards from "./components/cards/cards";
 
 function App() {
+  const categoryInits = [
+    { id: "1", title: "homme", description: "Vetement homme", image: "1001" },
+    { id: "2", title: "femme", description: "Vetement femme", image: "1002" },
+  ];
+
+  const categoryElements = categoryInits.map((category) => (
+    <div className="col-12 col-lg-3 " key={category.id}>
+      <Cards
+        title={category.title}
+        description={category.description}
+        image={category.image}
+      />
+    </div>
+  ));
+
   return (
-   <>
-<Page1/>
-<Cards/>
-   </>
+    <>
+      <Page1 />
+      <div className="row">{categoryElements}</div>
+    </>
   );
 }
 
--- exo1/src/components/cards/cards.jsx
+++ exo1/src/components/cards/cards.jsx
@@ -1,14 +1,15 @@
 
 
-function Cards() {
+const Cards = ({title, description, image}) => {
+
     return(
     <>
-        <div className="ms-5 card col-2">
+        <div className="ms-5 card">
           <img src="https://picsum.photos/seed/picsum/200/300" className="card-img-top" alt="..."/>
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="#" className="btn btn-primary">Allez</a>
           </div>
         </div>
             </>
